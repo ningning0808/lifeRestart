f160011dadea44366dab489250d427cdbbfee2fd
nbqq <90015236+nbqq@users.noreply.github.com>
2021-09-03 04:28:57 -0600

    更改错字

--- src/app.js
+++ src/app.js
@@ -293,7 +293,7 @@ class App{
                 <li><span>享年：</span>3岁 早夭</li>
                 <li><span>快乐：</span>3级 不太幸福的人生</li>
             </ul>
-            <div class="head" style="top:auto; bottom:35%">天赋，你可以选一个，下辈子还能抽到</div>
+            <div class="head" style="top:auto; bottom:35%">天赋，你可以选2个，下辈子还能抽到</div>
             <ul id="talents" class="selectlist" style="top:calc(65% + 0.5rem); bottom:8rem">
                 <li class="grade2b">黑幕（面试一定成功）</li>
             </ul>
